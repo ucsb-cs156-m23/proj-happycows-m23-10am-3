3d405628a3e57d2c792321c6b547ea95fae77675
xingyelin <xingyelin@umail.ucsb.edu>
2023-08-25 04:01:13 -0700

    cl - update profit controller

--- src/main/java/edu/ucsb/cs156/happiercows/controllers/ProfitsController.java
+++ src/main/java/edu/ucsb/cs156/happiercows/controllers/ProfitsController.java
@@ -9,8 +9,12 @@ import edu.ucsb.cs156.happiercows.repositories.UserCommonsRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
