ce40867872c4f1b23befb0cf4959b84b24ab77a8
Ryan He <ryanhe@ucsb.edu>
2023-08-26 10:39:20 -0700

    List commons now shows capacityPerUser and effectiveCapacity in the List Commons Table

--- frontend/src/main/components/Commons/CommonsTable.js
+++ frontend/src/main/components/Commons/CommonsTable.js
@@ -112,6 +112,15 @@ export default function CommonsTable({ commons, currentUser }) {
             Header: 'Carrying Capacity',
             accessor: row => row.commons.carryingCapacity,
             id: 'commons.carryingCapacity'
+        },
+        {
+            Header: 'Capacity Per User',
+            accessor: row => row.commons.capacityPerUser,
+            id: 'commons.capacityPerUser'
+        },
+        {
+            Header: 'Effective Capacity',
+            accessor: 'effectiveCapacity'
         }
     ];
 
